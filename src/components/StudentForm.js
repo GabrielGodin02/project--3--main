5210ecc3b951c8fd5c7b6cd8a7dda2e671ea28ed
Gabriel <gabrielgodin02@gmail.com>
2025-09-01 14:15:58 -0500

    version9

--- src/components/StudentForm.js
+++ src/components/StudentForm.js
@@ -124,6 +124,24 @@ const StudentForm = ({ onStudentChange }) => {
               </select>
             </td>
           </tr>
+          <tr>
+            <td style={{ padding: '10px' }}>
+              <label style={{ fontWeight: '500' }}>Jornada:</label>
+            </td>
+            <td style={{ padding: '10px' }}>
+              <select
+                name="schedule"
+                value={student.schedule}
+                onChange={handleChange}
+                style={inputStyle}
+              >
+                <option value="">Seleccione...</option>
+                <option value="Mañana">Mañana</option>
+                <option value="Tarde">Tarde</option>
+                <option value="Completa">Completa</option>
+              </select>
+            </td>
+          </tr>
         </tbody>
       </table>
 
